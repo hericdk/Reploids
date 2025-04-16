0c1c0488e6b9eb5a2939c810e32a1662dad0184b
heric <heric@raincake.com.br>
2025-04-16 05:04:24 +0000

    correção menu

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include "headers/utils.h"
-#include "headers/menu.h" // Agora o menu é incluído corretamente
-#include "telas.h"        // Inclui as telas
+#include "headers/menu.h"
+#include "telas.h"
 
-int telaAtual = 1;      // Começa na tela 1
-bool menuAtivo = false; // Controla se o menu está aberto
+int telaAtual = 1;
+bool menuAtivo = false;
+int opcaoSelecionada = 1;
+unsigned long lastMenuChange = 0;
+const unsigned long menuDebounceDelay = 200;
 
-int opcaoSelecionada = 1; // Variavel compartilhada com o menu.cpp
 
 void setup()
 {
@@ -16,40 +18,40 @@ void setup()
     cfg.internal_imu = true;
     M5.begin(cfg);
 
-    M5.Display.fillScreen(TFT_BLACK);
-    M5.Display.setTextColor(TFT_WHITE);
-    M5.Display.setTextSize(1);
+    Wire.begin(38, 39); 
 
     setupServos();
 
+    M5.Display.fillScreen(TFT_BLACK);
+    M5.Display.setTextColor(TFT_WHITE);
+    M5.Display.setTextSize(1);
 }
 
 void loop()
 {
     M5.update();
 
-    // **Se segurar o botão por mais de 200ms, ativa o menu**
-    if (!menuAtivo && M5.BtnA.pressedFor(200))
+     //Entrada no Menu
+    if (!menuAtivo && M5.BtnA.pressedFor(menuDebounceDelay))
     {
         menuAtivo = true;
-        opcaoSelecionada = 1; // Reinicia a seleção do menu
+        opcaoSelecionada = 1; 
         menu();
-     }
+    }
 
     if (menuAtivo)
     {
-        if (M5.BtnA.pressedFor(200))
+        if (M5.BtnA.pressedFor(menuDebounceDelay))
         {
-            // Sai do menu e entra na tela selecionada
             menuAtivo = false;
             telaAtual = opcaoSelecionada;
-        } 
-        else if (M5.BtnA.wasReleased()) // Agora espera soltar o botão antes de mudar de opção
+        } else if (M5.BtnA.wasReleased() && (millis() - lastMenuChange > menuDebounceDelay))
         {
             opcaoSelecionada++;
             if (opcaoSelecionada > 5)
-                opcaoSelecionada = 1; // Volta para a primeira opção se passar da última
-            menu();                   // Atualiza a tela do menu
+                opcaoSelecionada = 1; 
+            menu(); 
+            lastMenuChange = millis();
         }
     }
     else
@@ -57,7 +59,7 @@ void loop()
         // **Chama a tela correta continuamente para manter a animação**
         switch (telaAtual)
         {
-        case 1:
+        case 1: 
             mic();
             break;
         case 2:
@@ -74,4 +76,3 @@ void loop()
             break;
         }
     }
-}
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,46 +1,27 @@
 #include <M5Unified.h>
 #include "headers/menu.h"
 
-void menu()
-{
+void menu() {
     M5.Display.fillScreen(TFT_BLACK);
-    M5.Display.setCursor(10, 10);
+    M5.Display.setTextColor(TFT_WHITE);
     M5.Display.setTextSize(1);
-    M5.Display.printf("Menu Principal\n\n");
+    M5.Display.setCursor(10, 10);
+    M5.Display.println("Menu Principal\n");
 
-    for (int i = 1; i <= 5; i++)
-    {
-        int posY = 10 + (i * 15); // Ajusta a posição vertical de cada opção
+    const char *menuItems[] = {"1. mic/Audio", "2. Cubo 3D BG", "3. Cubo 3D", "4. Servo Motor", "5. Debug"};
+
+    for (int i = 0; i < 5; i++) {
+        int posY = 30 + (i * 10); // Ajusta a posição vertical de cada opção
         M5.Display.setCursor(20, posY);
 
-        if (i == ::opcaoSelecionada)
-        {
+        if ((i + 1) == opcaoSelecionada) {
             M5.Display.setTextColor(TFT_BLACK, TFT_WHITE);
-            M5.Display.fillRect(15, posY - 2, 100, 12, TFT_WHITE); // Cria um fundo branco
-        }
-        else
-        {
+            M5.Display.fillRect(15, posY, 100, 10, TFT_WHITE); // Cria um fundo branco
+        } else {
             M5.Display.setTextColor(TFT_WHITE);
         }
 
-        switch (i)
-        {
-        case 1:
-            M5.Display.printf("1. mic/Audio");
-            break;
-        case 2:
-            M5.Display.printf("2. Cubo 3D BG");
-            break;
-        case 3:
-            M5.Display.printf("3. Cubo 3D");
-            break;
-        case 4:
-            M5.Display.printf("4. Servo Motor");
-            break;
-        case 5:
-            M5.Display.printf("5. Debug");
-            break;
-        }
+        M5.Display.println(menuItems[i]);
     }
     M5.Display.setTextColor(TFT_WHITE);
 }
